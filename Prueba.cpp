b767c5e91cc8b13bde2a70810998e3d46aec771e
Facundo <facu_delsanto_peola@hotmail.com>
2020-12-12 22:58:43 -0300

    agregue structs

--- Prueba.cpp
+++ Prueba.cpp
@@ -1,10 +1,46 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <conio.h>
-
-struct
+#include <string.h>
+struct fecha
 {
-	printf ("HOla gato tonto");
-	printf ("otro cambio");
-	
+    int dia;
+    int mes;
+    int anio;
+};
+
+struct veterinario
+{
+    char apynom[60];
+    int matricula;
+    char contrasenia[10]; 
+    int dni;
+    char telefono[25];
+};
+
+struct usuario
+{
+    char usu[60];
+    char contrasenia[60];
+    char apynom[60];
+    int cp;
+};
+
+struct turnos
+{
+    int mat;
+    fecha fn;
+    int dni;
+    char detalle[380];
+};
+
+struct mascota
+{
+    char apynom[60];
+    char domicilio[60];
+    int dni;
+    char localidad[60];
+    fecha fec;
+    float peso;
+    char tel[25];
 };
